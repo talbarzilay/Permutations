ed3ec6c4d3dba535d72048e2bd1fb058d24d838b
Tal Barzilay <talbarzi@post.bgu.ac.il>
2021-10-24 12:44:16 +0300

    added initial code skeleton interface for the code, and the basic given tests.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("This is a test line!");
+//This file contains the code to the permutations function.
+
+
+/**
+ * return all unique permutations of the chars of the given string
+ * @param {*} string the string to permut its chars
+ * @returns an arry of all the permutatoins of the given string, only uniques. 
+ */
+function permutations(string) {
+    return [];
+}
+
+// ----------------------------------------- "private" methods -----------------------------------------
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,14 @@
 const index = require('../src/index');
 
-test('testing test', () => {
-  expect(true).toBe(true);
-});
+// test the permutations functions
+describe('permutations', function() {
+    
+    //This are the example tests for the code
+    it('examples from description', function() {
+      Test.assertSimilar(permutations('a'), ['a']);
+      Test.assertSimilar(permutations('ab').sort(), ['ab', 'ba'].sort());
+      Test.assertSimilar(permutations('aabb').sort(), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'].sort());
+    });
+  });
+
+// ----------------------------------------- "private" methods tests -----------------------------------------
